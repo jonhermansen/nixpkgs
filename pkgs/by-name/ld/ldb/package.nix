5ed1d3c3acc87e478ff629ad520ba5402fd9859e
Robert Scott <code@humanleg.org.uk>
2025-01-12 18:58:33 +0000

    ldb: fix pkgsLLVM build

--- pkgs/by-name/ld/ldb/package.nix
+++ pkgs/by-name/ld/ldb/package.nix
@@ -73,6 +73,9 @@ stdenv.mkDerivation (finalAttrs: {
   # module, which works correctly in all cases.
   PYTHON_CONFIG = "/invalid";
 
+  # https://reviews.llvm.org/D135402
+  NIX_LDFLAGS = lib.optional (stdenv.cc.bintools.isLLVM && lib.versionAtLeast stdenv.cc.bintools.version "17") "--undefined-version";
+
   stripDebugList = [ "bin" "lib" "modules" ];
 
   passthru.tests.pkg-config = testers.hasPkgConfigModules {
