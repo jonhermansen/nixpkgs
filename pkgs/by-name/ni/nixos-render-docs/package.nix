627592f3fe418a89672c905dd1a0c26ec6f024a7
Audrey Dutcher <audrey@rhelmot.io>
2025-02-11 07:34:43 -0700

    nixos-render-docs: move pytestCheckHook to nativeCheckInputs
    Previously, it was in nativeBuildInputs which caused tests to run while
    cross compiling. This attempts to run host python on the build machine.

--- pkgs/by-name/ni/nixos-render-docs/package.nix
+++ pkgs/by-name/ni/nixos-render-docs/package.nix
@@ -38,9 +38,12 @@ python.pkgs.buildPythonApplication rec {
     src = ./src;
   };
 
-  nativeBuildInputs = with python.pkgs; [
-    setuptools
-    pytestCheckHook
+  nativeCheckInputs = [
+    python.pkgs.pytestCheckHook
+  ];
+
+  nativeBuildInputs = [
+    python.pkgs.setuptools
   ];
 
   propagatedBuildInputs = with python.pkgs; [
