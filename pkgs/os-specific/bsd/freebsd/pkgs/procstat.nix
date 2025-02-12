5e0e739291dbe44f3bd094131f92fa00e3f68e97
Audrey Dutcher <audrey@rhelmot.io>
2025-02-11 21:22:25 -0700

    freebsd.procstat: init

---
+++ pkgs/os-specific/bsd/freebsd/pkgs/procstat.nix
@@ -0,0 +1,16 @@
+{
+  mkDerivation,
+  libxo,
+  libsbuf,
+}:
+mkDerivation {
+  path = "usr.bin/procstat";
+  buildInputs = [
+    libxo
+    libsbuf
+  ];
+
+  outputs = [ "out" "man" "debug" ];
+
+  MK_TESTS = "no";
+}
