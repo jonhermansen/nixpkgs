20aef22db522ab8a1f4b31aec92f46cc8d18611f
Audrey Dutcher <audrey@rhelmot.io>
2025-02-20 19:59:19 -0700

    Revert "[squashme] jail should replace some paths"
    This reverts commit 4fc5f6f0a284699ecbcb13c9a046d3d5c99ecdbd.

--- pkgs/os-specific/bsd/freebsd/pkgs/jail.nix
+++ pkgs/os-specific/bsd/freebsd/pkgs/jail.nix
@@ -14,9 +14,6 @@ mkDerivation {
   buildInputs = [
     libjail
   ];
-  postPatch = ''
-    substituteInPlace $BSDSRCDIR/usr.sbin/jail/command.c --replace "/sbin/" "" --replace _PATH_MOUNT '"mount"'
-  '';
   MK_TESTS = "no";
   meta.mainProgram = "jail";
   meta.platforms = lib.platforms.freebsd;
