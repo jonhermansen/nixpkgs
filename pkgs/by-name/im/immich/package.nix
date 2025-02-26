d7070834965382a4d99a71498a1a0b03eae71f24
Robert Schütz <nix@dotlambda.de>
2025-02-26 08:36:25 -0800

    immich: 1.126.1 -> 1.127.0
    Diff: https://github.com/immich-app/immich/compare/refs/tags/v1.126.1...v1.127.0
    
    Changelog: https://github.com/immich-app/immich/releases/tag/v1.127.0

--- pkgs/by-name/im/immich/package.nix
+++ pkgs/by-name/im/immich/package.nix
@@ -18,12 +18,19 @@
   cacert,
   unzip,
   # runtime deps
+  cairo,
   exiftool,
+  giflib,
   jellyfin-ffmpeg, # Immich depends on the jellyfin customizations, see https://github.com/NixOS/nixpkgs/issues/351943
   imagemagick,
+  libjpeg,
+  libpng,
   libraw,
   libheif,
+  librsvg,
+  pango,
   perl,
+  pixman,
   vips,
 }:
 let
@@ -117,6 +124,23 @@ let
       rm node_modules/@immich/sdk
       ln -s ${openapi} node_modules/@immich/sdk
     '';
+
+    env.npm_config_build_from_source = "true";
+
+    nativeBuildInputs = [
+      pkg-config
+    ];
+
+    buildInputs = [
+      # https://github.com/Automattic/node-canvas/blob/master/Readme.md#compiling
+      cairo
+      giflib
+      libjpeg
+      libpng
+      librsvg
+      pango
+      pixman
+    ];
 
     installPhase = ''
       runHook preInstall
