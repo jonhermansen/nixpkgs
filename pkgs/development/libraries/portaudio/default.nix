da17096575e28c86b763f47955b42126fff08dc3
Audrey Dutcher <audrey@rhelmot.io>
2025-03-06 02:26:52 -0700

    portaudio: fix build for FreeBSD

--- pkgs/development/libraries/portaudio/default.nix
+++ pkgs/development/libraries/portaudio/default.nix
@@ -24,7 +24,9 @@ stdenv.mkDerivation rec {
   ];
   buildInputs = [
     libjack2
-  ] ++ lib.optionals (lib.meta.availableOn stdenv.hostPlatform alsa-lib) [ alsa-lib ];
+  ]
+  # Enabling alsa causes linux-only sources to be built
+  ++ lib.optionals stdenv.hostPlatform.isLinux [ alsa-lib ];
 
   configureFlags = [
     "--disable-mac-universal"
@@ -50,7 +52,7 @@ stdenv.mkDerivation rec {
     ''
       make install
     ''
-    + lib.optionalString (lib.meta.availableOn stdenv.hostPlatform alsa-lib) ''
+    + lib.optionalString stdenv.hostPlatform.isLinux ''
       # fixup .pc file to find alsa library
       sed -i "s|-lasound|-L${alsa-lib.out}/lib -lasound|" "$out/lib/pkgconfig/"*.pc
     ''
