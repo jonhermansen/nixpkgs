d7a485ffb5f4e0c0feb7acef21303345efd8d2b5
Audrey Dutcher <audrey@rhelmot.io>
2025-05-26 09:18:33 -0700

    wmenu: enable FreeBSD

--- pkgs/by-name/wm/wmenu/package.nix
+++ pkgs/by-name/wm/wmenu/package.nix
@@ -47,7 +47,7 @@ stdenv.mkDerivation rec {
     description = "Efficient dynamic menu for Sway and wlroots based Wayland compositors";
     homepage = "https://codeberg.org/adnano/wmenu";
     license = licenses.mit;
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.freebsd;
     maintainers = with maintainers; [ eken ];
     mainProgram = "wmenu";
   };
