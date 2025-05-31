9ef0d1808b5205baa5e05d448e6dc8712c565324
Audrey Dutcher <audrey@rhelmot.io>
2025-05-31 01:45:17 -0700

    wayland-utils: fix cross build for FreeBSD

--- pkgs/by-name/wa/wayland-utils/package.nix
+++ pkgs/by-name/wa/wayland-utils/package.nix
@@ -43,7 +43,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = "https://gitlab.freedesktop.org/wayland/wayland-utils";
     license = licenses.mit; # Expat version
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.freebsd;
     maintainers = with maintainers; [ primeos ];
     mainProgram = "wayland-info";
   };
