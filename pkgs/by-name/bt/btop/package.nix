04401b00f45117f192697fd3212ab96ce863e506
Audrey Dutcher <audrey@rhelmot.io>
2025-05-31 01:46:06 -0700

    btop: fix cross build for FreeBSD

--- pkgs/by-name/bt/btop/package.nix
+++ pkgs/by-name/bt/btop/package.nix
@@ -7,6 +7,7 @@
   removeReferencesTo,
   autoAddDriverRunpath,
   apple-sdk_15,
+  freebsd,
   versionCheckHook,
   rocmPackages,
   cudaSupport ? config.cudaSupport,
@@ -34,6 +35,9 @@ stdenv.mkDerivation rec {
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     apple-sdk_15
+  ] ++ lib.optionals stdenv.hostPlatform.isFreeBSD [
+    freebsd.libproc
+    freebsd.librtld-db
   ];
 
   installFlags = [ "PREFIX=$(out)" ];
@@ -56,7 +60,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/aristocratos/btop";
     changelog = "https://github.com/aristocratos/btop/blob/v${version}/CHANGELOG.md";
     license = lib.licenses.asl20;
-    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin ++ lib.platforms.freebsd;
     maintainers = with lib.maintainers; [
       khaneliman
       rmcgibbo
