fc94eb494d23face6dcf97e91f8642b535e09d77
Jon Hermansen <jon@jh86.org>
2025-10-25 14:38:59 -0400

    more 15.0 kernel shizz

--- pkgs/os-specific/bsd/freebsd/pkgs/sys/package.nix
+++ pkgs/os-specific/bsd/freebsd/pkgs/sys/package.nix
@@ -49,11 +49,11 @@ let
     ];
     postPatch =
       ''
-        for f in sys/conf/kmod.mk sys/contrib/dev/acpica/acpica_prep.sh; do
+        for f in sys/conf/kmod.mk sys/contrib/dev/acpica/acpica_prep.sh sys/contrib/dev/athk/ath12k/zzz_fw_ports_fwget.sh sys/contrib/dev/athk/ath11k/zzz_fw_ports_fwget.sh; do
           substituteInPlace "$f" --replace-warn 'xargs -J' 'xargs-j '
         done
-
         for f in sys/conf/*.mk; do
+          substituteInPlace "$f" --replace-quiet '\${XARGS} \${XARGS_J}' 'xargs-j'
           substituteInPlace "$f" --replace-quiet 'KERN_DEBUGDIR}''${' 'KERN_DEBUGDIR_'
         done
       ''
@@ -61,7 +61,6 @@ let
         cat ${baseConfigFile} >>sys/${hostArchBsd}/conf/${baseConfig}
       '';
   };
-
   # Kernel modules need this for kern.opts.mk
   env =
     {
@@ -76,12 +75,10 @@ let
 in
 mkDerivation rec {
   pname = "sys";
-
   # Patch source outside of this derivation so out-of-tree modules can use it
   src = patchedSource;
   path = "sys";
   autoPickPatches = false;
-
   nativeBuildInputs = [
     bsdSetupHook
     mandoc
@@ -99,55 +96,41 @@ mkDerivation rec {
     ctfconvert
     ctfmerge
   ];
-
   # --dynamic-linker /red/herring is used when building the kernel.
   NIX_ENFORCE_PURITY = 0;
-
   AWK = "${buildPackages.gawk}/bin/awk";
-
   CWARNEXTRA = "-Wno-error=shift-negative-value -Wno-address-of-packed-member";
-
   hardeningDisable = [
     "pic" # generates relocations the linker can't handle
     "stackprotector" # generates stack protection for the function generating the stack canary
   ];
-
   # hardeningDisable = stackprotector doesn't seem to be enough, put it in cflags too
   NIX_CFLAGS_COMPILE = [
     "-fno-stack-protector"
     "-Wno-unneeded-internal-declaration" # some openzfs code trips this
   ];
-
   inherit env;
   passthru.env = env;
-
   KODIR = "${builtins.placeholder "out"}/kernel";
   KMODDIR = "${builtins.placeholder "out"}/kernel";
   DTBDIR = "${builtins.placeholder "out"}/dbt";
-
   KERN_DEBUGDIR = "${builtins.placeholder "debug"}/lib/debug";
   KERN_DEBUGDIR_KODIR = "${KERN_DEBUGDIR}/kernel";
   KERN_DEBUGDIR_KMODDIR = "${KERN_DEBUGDIR}/kernel";
-
   skipIncludesPhase = true;
-
   configurePhase = ''
     runHook preConfigure
-
     cd ${hostArchBsd}/conf
     config ${baseConfig}
-
     runHook postConfigure
   '';
   preBuild = ''
     cd ../compile/${baseConfig}
   '';
-
   outputs = [
     "out"
     "debug"
   ];
-
   meta = {
     description = "FreeBSD kernel and modules";
     platforms = lib.platforms.freebsd;
