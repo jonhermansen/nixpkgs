abe6f45e69f17afc4dd022289f4c29b0c6898995
Jon Hermansen <jon@jh86.org>
2025-10-27 11:37:21 -0400

    fix infinite recursion due to sys -> git -> sys loop

--- pkgs/os-specific/bsd/freebsd/pkgs/source.nix
+++ pkgs/os-specific/bsd/freebsd/pkgs/source.nix
@@ -1,9 +1,9 @@
-{ fetchgit, sourceData }:
+{ fetchzip, sourceData }:
 
 # Using fetchFromGitHub from their mirror because it's a lot faster than their git server
 # If you want you could fetchgit from "https://git.FreeBSD.org/src.git" instead.
 # The update script still pulls directly from git.freebsd.org
-fetchgit {
-  url = "https://git.FreeBSD.org/src.git";
-  inherit (sourceData) rev hash;
+fetchzip {
+  url = "https://github.com/freebsd/freebsd-src/archive/c8918d6c7412fce87922e9bd7e4f5c7d7ca96eb7.tar.gz";
+  inherit (sourceData) hash;
 }
