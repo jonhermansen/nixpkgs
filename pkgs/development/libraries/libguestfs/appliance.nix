c6be9ce2c1ae9e7606f797af72d6cfb73955c1ea
Jon Hermansen <jon@jh86.org>
2026-02-01 13:30:20 -0500

    libguestfs-appliance: fix source URL

--- pkgs/development/libraries/libguestfs/appliance.nix
+++ pkgs/development/libraries/libguestfs/appliance.nix
@@ -2,7 +2,7 @@
 
 fetchzip {
   name = "libguestfs-appliance-1.40.1";
-  url = "http://download.libguestfs.org/binaries/appliance/appliance-1.40.1.tar.xz";
+  url = "https://web.archive.org/web/20230826201338if_/https://download.libguestfs.org/binaries/appliance/appliance-1.40.1.tar.xz";
   sha256 = "00863mm08p55cv6w8awp7y0lv894rcrm70mjwqfc8nc4yyb70xlm";
 
   meta = {
