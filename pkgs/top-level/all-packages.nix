9c922aab4a59949bcf5b991cbaedfa1a545bc185
Jon Hermansen <jon.hermansen@obsidian.systems>
2026-03-26 22:11:34 -0400

    k3s_1_30: bring it back from the dead

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10703,6 +10703,7 @@ with pkgs;
   jwm-settings-manager = callPackage ../applications/window-managers/jwm/jwm-settings-manager.nix { };
 
   inherit (callPackage ../applications/networking/cluster/k3s { })
+    k3s_1_30
     k3s_1_32
     k3s_1_33
     k3s_1_34
